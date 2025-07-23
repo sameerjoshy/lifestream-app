550dd0343247bf4f604ba44ea873a5838aa81037
sameerjoshy <sameer@growth-engineer.pro>
2025-07-23 11:28:41 +0530

    Update api-keys.js

--- js/config/api-keys.js
+++ js/config/api-keys.js
@@ -1,5 +1,6 @@
 const API_CONFIG = {
     GEMINI_API_KEY: 'AIzaSyDZT_EPRBIMrkkaHhH6VUnECgjR2yqKJ0U',
+    GEMINI_MODEL: 'gemini-1.5-flash',
     ENVIRONMENT: 'development'
 };
 window.LIFESTREAM_CONFIG = API_CONFIG;
